41df0081384a910282782d6239a801c9cd4b473a
Hugo Araya Carrasco <hugo.araya.carrasco@gmail.com>
2022-09-01 21:29:54 -0400

    g

---
+++ dos.py
@@ -0,0 +1,8 @@
+# Resolver ecuacuiones lineales de la forma ax+b = c
+
+a = float(input('Coeficente a: '))
+b = float(input('Coeficente b: '))
+c = float(input('Coeficente c: '))
+
+x = (c - b)/a
+print('x = ',x)
---
+++ uno.py
@@ -0,0 +1,3 @@
+nombre = input('Ingrese su nombre: ')
+saludar = 'Buenas noches estimado '+nombre
+print(saludar)
